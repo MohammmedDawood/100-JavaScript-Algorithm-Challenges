be094fc33f30fb430c28a5b266859d339ad25b86
Mohammed Dawood <mahmedtony73@gmail.com>
2021-11-05 04:42:05 +0200

    All Longest Strings

--- allLongestStrings/allLongestStrings.ts
+++ allLongestStrings/allLongestStrings.ts
@@ -1,5 +1,15 @@
 function allLongestStrings(inputArray: string[]): string[] {
-
+  let longestString: string = "";
+  let longestStringArray: string[] = [];
+  for (let i = 0; i < inputArray.length; i++) {
+    if (inputArray[i].length > longestString.length) {
+      longestString = inputArray[i];
+      longestStringArray = [longestString];
+    } else if (inputArray[i].length === longestString.length) {
+      longestStringArray.push(inputArray[i]);
+    }
+  }
+  return longestStringArray;
 }
 
 console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]));
