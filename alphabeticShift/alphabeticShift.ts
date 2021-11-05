495c1d488de0c435792d24238a5cad79ffa31357
Mohammed Dawood <mahmedtony73@gmail.com>
2021-11-05 13:31:33 +0200

    Alphabetic Shift

--- alphabeticShift/alphabeticShift.ts
+++ alphabeticShift/alphabeticShift.ts
@@ -1,5 +1,16 @@
 function alphabeticShift(inputString: string): string {
+  const alphabet: string[] = "abcdefghijklmnopqrstuvwxyz".split("");
 
+  let inputShifted = inputString.split("");
+
+  for (let i = 0; i < inputShifted.length; i++) {
+    if (inputShifted[i] === "z") {
+      inputShifted[i] = "a";
+    } else {
+      inputShifted[i] = alphabet[alphabet.indexOf(inputShifted[i]) + 1];
+    }
+  }
+  return inputShifted.join("");
 }
 
-console.log(alphabeticShift('crazy'));
+console.log(alphabeticShift("crazy"));
