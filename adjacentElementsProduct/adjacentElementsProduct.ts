415bfbaf08726035a9c81b4bff717179eef69f16
Mohammed Dawood <mahmedtony73@gmail.com>
2021-11-05 04:36:06 +0200

    Adjacent Elements Product

--- adjacentElementsProduct/adjacentElementsProduct.ts
+++ adjacentElementsProduct/adjacentElementsProduct.ts
@@ -9,4 +9,4 @@ function adjacentElementsProduct(inputArray: number[]): number {
   return max;
 }
 
-//console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
+console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
