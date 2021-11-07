12d163b94fca023a5373c742c1f30fe07ce38b12
Mohammed Dawood <mahmedtony73@gmail.com>
2021-11-07 05:14:01 +0200

    Check Palindrome

--- checkPalindrome/checkPalindrome.ts
+++ checkPalindrome/checkPalindrome.ts
@@ -1,7 +1,12 @@
 function checkPalindrome(inputString: string): boolean {
-
+  let input = inputString.toLowerCase();
+  let reverse = input.split("").reverse().join("");
+  if (input === reverse) {
+    return true;
+  }
+  return false;
 }
 
-console.log(checkPalindrome('aabaa'));
-console.log(checkPalindrome('abac'));
-console.log(checkPalindrome('a'));
+console.log(checkPalindrome("aabaa"));
+console.log(checkPalindrome("abac"));
+console.log(checkPalindrome("a"));
