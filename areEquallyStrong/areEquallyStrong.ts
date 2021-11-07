6102a95ebd2b98c8c24f171ecb1569c9f343cf69
Mohammed Dawood <mahmedtony73@gmail.com>
2021-11-07 04:53:05 +0200

    Are Equally Strong

--- areEquallyStrong/areEquallyStrong.ts
+++ areEquallyStrong/areEquallyStrong.ts
@@ -1,7 +1,16 @@
-function areEquallyStrong(yourLeft: number, yourRight: number, friendsLeft: number, friendsRight: number): boolean {
-
+function areEquallyStrong(
+  yourLeft: number,
+  yourRight: number,
+  friendsLeft: number,
+  friendsRight: number
+): boolean {
+  const yourMax = Math.max(yourLeft, yourRight);
+  const yourMin = Math.min(yourLeft, yourRight);
+  const friendsMax = Math.max(friendsLeft, friendsRight);
+  const friendsMin = Math.min(friendsLeft, friendsRight);
+  return yourMax === friendsMax && yourMin === friendsMin;
 }
 
-// console.log(areEquallyStrong(10, 15, 15, 10))
-// console.log(areEquallyStrong(15, 10, 15, 10))
-// console.log(areEquallyStrong(15, 10, 15, 9))
+console.log(areEquallyStrong(10, 15, 15, 10));
+console.log(areEquallyStrong(15, 10, 15, 10));
+console.log(areEquallyStrong(15, 10, 15, 9));
