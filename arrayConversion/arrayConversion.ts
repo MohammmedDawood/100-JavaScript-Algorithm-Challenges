d11c90799a8c060f2ce2943da35031d7c658237e
Mohammed Dawood <mahmedtony73@gmail.com>
2021-11-07 05:57:25 +0200

    Array Conversion

--- arrayConversion/arrayConversion.ts
+++ arrayConversion/arrayConversion.ts
@@ -1,5 +1,20 @@
-function arrayConversion(inputArray: number[]): number {
-
+function arrayConversion(inputArray: Number[], operation: string): Number[] {
+  if (inputArray.length === 1) {
+    return inputArray;
+  } else {
+    let result = [];
+    for (let i = 0; i < inputArray.length; i += 2) {
+      let element: Number;
+      if (operation === undefined || operation === "sum") {
+        element = inputArray[i] + inputArray[i + 1];
+      } else if (operation === "multiply") {
+        element = inputArray[i] * inputArray[i + 1];
+      }
+      result.push(element);
+    }
+    operation = operation === "multiply" ? "sum" : "multiply";
+    return arrayConversion(result, operation);
+  }
 }
 
 console.log(arrayConversion([1, 2, 3, 4, 5, 6, 7, 8]));
