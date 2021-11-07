0cb8546cc093f022722e3d15278b569bd5b9595a
Mohammed Dawood <mahmedtony73@gmail.com>
2021-11-07 06:39:09 +0200

    Array Replace

--- arrayReplace/arrayReplace.ts
+++ arrayReplace/arrayReplace.ts
@@ -1,5 +1,9 @@
-function arrayReplace(inputArray: number[], elemToReplace: number, substitutionElem: number): number[] {
-   
+function arrayReplace(
+  inputArray: number[],
+  elemToReplace: number,
+  substitutionElem: number
+): number[] {
+  return inputArray.map((x) => (x === elemToReplace ? substitutionElem : x));
 }
 
 console.log(arrayReplace([1, 2, 1], 1, 3));
