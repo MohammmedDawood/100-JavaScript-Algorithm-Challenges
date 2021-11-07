9ce938a64fb05f547214e2c3f77cdf439ce361a2
Mohammed Dawood <mahmedtony73@gmail.com>
2021-11-07 06:58:37 +0200

    Avoid Obstacles

--- avoidObstacles/avoidObstacles.ts
+++ avoidObstacles/avoidObstacles.ts
@@ -1,5 +1,12 @@
 function avoidObstacles(inputArray: number[]): number {
-
+  let result = 1;
+  let max = Math.max(...inputArray);
+  for (let i = 1; i <= max; i++) {
+    if (inputArray.every((x) => x % i !== 0)) {
+      result = i;
+      return result;
+    }
+  }
 }
 
 console.log(avoidObstacles([5, 3, 6, 7, 9]));
