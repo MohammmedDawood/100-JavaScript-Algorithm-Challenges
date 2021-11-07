ddd1eeb4c6df797946d5485e4294ba3d9c5d98cc
Mohammed Dawood <mahmedtony73@gmail.com>
2021-11-07 07:29:54 +0200

    Case Insensitive Palindrome

--- caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
+++ caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
@@ -1,6 +1,7 @@
 function isCaseInsensitivePalindrome(inputString: string): boolean {
-
+  const reversed = inputString.split("").reverse().join("");
+  return inputString.toLowerCase() === reversed.toLowerCase();
 }
 
-console.log(isCaseInsensitivePalindrome('AaBaa'));
-console.log(isCaseInsensitivePalindrome('abac'));
+console.log(isCaseInsensitivePalindrome("AaBaa"));
+console.log(isCaseInsensitivePalindrome("abac"));
