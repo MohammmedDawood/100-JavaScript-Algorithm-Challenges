f5ed73dd18acf56f02893c97f9a4c1e118840f6c
Mohammed Dawood <mahmedtony73@gmail.com>
2021-11-07 07:55:34 +0200

    Common Character Count

--- commonCharacterCount/commonCharacterCount.ts
+++ commonCharacterCount/commonCharacterCount.ts
@@ -1,5 +1,14 @@
 function commonCharacterCount(s1: string, s2: string): number {
-   
+  let count = 0;
+  let s1Arr = s1.split("");
+  let s2Arr = s2.split("");
+  for (let i = 0; i < s1Arr.length; i++) {
+    if (s2Arr.includes(s1Arr[i])) {
+      count++;
+      s2Arr.splice(s2Arr.indexOf(s1Arr[i]), 1);
+    }
+  }
+  return count;
 }
 
-console.log(commonCharacterCount('aabcc', 'adcaa'));
+console.log(commonCharacterCount("aabcc", "adcaa"));
