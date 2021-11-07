ebbc07c88368cad5c4ea1fff3ae25fc80840e4cc
Mohammed Dawood <mahmedtony73@gmail.com>
2021-11-07 06:16:42 +0200

    Array Max Consecutive Sum

--- arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.ts
+++ arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.ts
@@ -1,5 +1,9 @@
 function arrayMaxConsecutiveSum(inputArray: number[], k: number): number {
-
+  let maxSum = [];
+  for (let i = 0; i < inputArray.length - 1; i++) {
+    maxSum.push(inputArray.slice(i, i + k));
+  }
+  return Math.max(...maxSum.map((x) => x.reduce((a, b) => a + b)));
 }
 
 console.log(arrayMaxConsecutiveSum([2, 3, 5, 1, 6], 2));
