ab08a68b59fca4eb1f7e55fd4957c7c517031cf6
Mohammed Dawood <mahmedtony73@gmail.com>
2021-11-07 16:05:46 +0200

    Convert String

--- convertString/convertString.ts
+++ convertString/convertString.ts
@@ -1,6 +1,19 @@
 function convertString(s: string, t: string): boolean {
+  let word = "";
+  let tIndex = 0;
+  // const sChars = s.split("");
 
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === t[tIndex]) {
+      word += s[i];
+      tIndex++;
+      if (word === t) {
+        return true;
+      }
+    }
+  }
+  return false;
 }
 
-console.log(convertString('ceoydefthf5iyg5h5yts', 'codefights'));
-console.log(convertString('addbyca', 'abcd'));
+console.log(convertString("ceoydefthf5iyg5h5yts", "codefights"));
+console.log(convertString("addbyca", "abcd"));
