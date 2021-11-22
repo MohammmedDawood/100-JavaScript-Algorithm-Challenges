08723da316166808b09fc9812ba53497bbd776ba
Mohammed Dawood <mahmedtony73@gmail.com>
2021-11-22 07:29:41 +0200

    Update fancyRide.ts

--- fancyRide/fancyRide.ts
+++ fancyRide/fancyRide.ts
@@ -1,5 +1,14 @@
 function fancyRide(l: number, fares: number[]): string {
-
+  let rides: string[] = ["UberX", "UberXL", "UberPlus", "UberBlack", "UberSUV"];
+  let affordableRide: string;
+  fares
+    .map((fare) => fare * l)
+    .filter((fare, index) => {
+      if (fare <= 20) {
+        affordableRide = rides[index];
+      }
+    });
+  return affordableRide;
 }
 
 console.log(fancyRide(30, [0.3, 0.5, 0.7, 1, 1.3]));
