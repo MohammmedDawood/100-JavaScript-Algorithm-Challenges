e678322cd715433d002e7fe84a44e5cf42cc9053
Mohammed Dawood <mahmedtony73@gmail.com>
2021-11-22 07:12:11 +0200

    Update extractMatrixColumn.ts

--- extractMatrixColumn/extractMatrixColumn.ts
+++ extractMatrixColumn/extractMatrixColumn.ts
@@ -1,5 +1,18 @@
 function extractMatrixColumn(matrix: number[][], column: number): number[] {
-
+  let columnVector: number[] = [];
+  for (let i: number = 0; i < matrix.length; i++) {
+    columnVector.push(matrix[i][column]);
+  }
+  return columnVector;
 }
 
-console.log(extractMatrixColumn([[1, 1, 1, 2], [0, 5, 0, 4], [2, 1, 3, 6]], 2));
+console.log(
+  extractMatrixColumn(
+    [
+      [1, 1, 1, 2],
+      [0, 5, 0, 4],
+      [2, 1, 3, 6],
+    ],
+    2
+  )
+);
