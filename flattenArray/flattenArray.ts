118c826ddaf715a55ec47e5df2ac094d1e587088
Mohammed Dawood <mahmedtony73@gmail.com>
2021-11-22 08:34:05 +0200

    Update flattenArray.ts

--- flattenArray/flattenArray.ts
+++ flattenArray/flattenArray.ts
@@ -1,5 +1,8 @@
 function flattenArray(arr: any[]): any[] {
-
+  return arr.reduce(
+    (a, b) => a.concat(Array.isArray(b) ? flattenArray(b) : b),
+    []
+  );
 }
 
 console.log(flattenArray([[["a"]], [["b"]]]));
