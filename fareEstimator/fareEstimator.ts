9c68dfdb19a8e7291a3f7b1ef75a4ef2ec3b6889
Mohammed Dawood <mahmedtony73@gmail.com>
2021-11-22 07:41:15 +0200

    Update fareEstimator.ts

--- fareEstimator/fareEstimator.ts
+++ fareEstimator/fareEstimator.ts
@@ -1,5 +1,21 @@
-function fareEstimator(ride_time: number, ride_distance: number, cost_per_minute: number[], cost_per_mile: number[]): number[] {
-
+function fareEstimator(
+  ride_time: number,
+  ride_distance: number,
+  cost_per_minute: number[],
+  cost_per_mile: number[]
+): number[] {
+  let totalCost: number[] = [];
+  for (let i = 0; i < cost_per_minute.length; i++) {
+    totalCost.push(
+      parseFloat(
+        (
+          cost_per_minute[i] * ride_time +
+          cost_per_mile[i] * ride_distance
+        ).toFixed(2)
+      )
+    );
+  }
+  return totalCost;
 }
 
 console.log(fareEstimator(30, 7, [0.2, 0.35, 0.4, 0.45], [1.1, 1.8, 2.3, 3.5]));
