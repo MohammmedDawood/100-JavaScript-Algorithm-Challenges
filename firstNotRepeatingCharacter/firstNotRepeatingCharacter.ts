f1634b658e510086286eb79ea04436d031e1c3b3
Mohammed Dawood <mahmedtony73@gmail.com>
2021-11-22 08:30:07 +0200

    Update firstNotRepeatingCharacter.ts

--- firstNotRepeatingCharacter/firstNotRepeatingCharacter.ts
+++ firstNotRepeatingCharacter/firstNotRepeatingCharacter.ts
@@ -1,6 +1,19 @@
 function firstNotRepeatingCharacter(s: string): string {
-
+  let dict: { [key: string]: number } = {};
+  for (let i = 0; i < s.length; i++) {
+    if (dict[s[i]]) {
+      dict[s[i]]++;
+    } else {
+      dict[s[i]] = 1;
+    }
+  }
+  for (let i = 0; i < s.length; i++) {
+    if (dict[s[i]] === 1) {
+      return s[i];
+    }
+  }
+  return "_";
 }
 
-console.log(firstNotRepeatingCharacter('abacabad'));
-console.log(firstNotRepeatingCharacter('abacabaabacaba'));
+console.log(firstNotRepeatingCharacter("abacabad"));
+console.log(firstNotRepeatingCharacter("abacabaabacaba"));
