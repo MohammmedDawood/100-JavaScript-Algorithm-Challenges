7986aec923011e20c4bb47f0848d1a9f18f69024
Mohammed Dawood <mahmedtony73@gmail.com>
2021-11-22 08:23:56 +0200

    Update findEmailDomain.ts

--- findEmailDomain/findEmailDomain.ts
+++ findEmailDomain/findEmailDomain.ts
@@ -1,6 +1,8 @@
 function findEmailDomain(address: string): string {
-
+  return address.lastIndexOf("@") > -1
+    ? address.substring(address.lastIndexOf("@") + 1)
+    : "";
 }
 
-console.log(findEmailDomain('prettyandsimple@example.com'));
-console.log(findEmailDomain('<>[]:,;@\"!#$%&*+-/=?^_{}| ~.a\"@example.org'));
+console.log(findEmailDomain("prettyandsimple@example.com"));
+console.log(findEmailDomain('<>[]:,;@"!#$%&*+-/=?^_{}| ~.a"@example.org'));
