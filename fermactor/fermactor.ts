41470514b4d4f24a61aef93b66966484dbfeaa4f
Mohammed Dawood <mahmedtony73@gmail.com>
2021-11-22 08:10:56 +0200

    Update fermactor.ts

--- fermactor/fermactor.ts
+++ fermactor/fermactor.ts
@@ -1,5 +1,12 @@
 function fermactor(n: number): number[] {
-
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      const total = i ** 2 - j ** 2;
+      if (total === n) {
+        return [i, j];
+      }
+    }
+  }
 }
 
 console.log(fermactor(15));
