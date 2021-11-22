2c6cfed75b63ca6be3d67a2690d0e43d2a144ac3
Mohammed Dawood <mahmedtony73@gmail.com>
2021-11-22 07:10:18 +0200

    Update extractEachKth.ts

--- extractEachKth/extractEachKth.ts
+++ extractEachKth/extractEachKth.ts
@@ -1,4 +1,13 @@
-function extractEachKth(inputArray:number[], k: number): number[] {
+function extractEachKth(inputArray: number[], k: number): number[] {
+  return inputArray.filter((value, index) => (index + 1) % k !== 0);
+
+  //   let result: number[] = [];
+  //   for (let i = 0; i < inputArray.length; i++) {
+  //     if ((i + 1) % k !== 0) {
+  //       result.push(inputArray[i]);
+  //     }
+  //   }
+  //   return result;
 }
 
 console.log(extractEachKth([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3));
