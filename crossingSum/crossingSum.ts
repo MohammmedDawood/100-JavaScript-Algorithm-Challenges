8f763c87ff1bc668c1e1ac4b1e13387a87bd1526
Mohammed Dawood <mahmedtony73@gmail.com>
2021-11-22 06:12:08 +0200

    Update crossingSum.ts

--- crossingSum/crossingSum.ts
+++ crossingSum/crossingSum.ts
@@ -1,7 +1,25 @@
 function crossingSum(matrix: number[][], a: number, b: number): number {
-  
+  const rowTotal = matrix[a].reduce((a, b) => a + b);
+  console.log(rowTotal);
+  let ColumnTotal = 0;
+  for (let i = 0; i < matrix.length; i++) {
+    if (i !== a) {
+      ColumnTotal += matrix[i][b];
+    }
+  }
+  console.log(ColumnTotal);
+
+  return rowTotal + ColumnTotal;
 }
 
-console.log(crossingSum([[1, 1, 1, 1], 
-    [2, 2, 2, 2], 
-    [3, 3, 3, 3]], 1, 3));
+console.log(
+  crossingSum(
+    [
+      [1, 1, 1, 1],
+      [2, 2, 2, 2],
+      [3, 3, 3, 3],
+    ],
+    1,
+    3
+  )
+);
