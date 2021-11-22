280fafa23a00f1e37a80f8468ec5f8d86a92add9
Mohammed Dawood <mahmedtony73@gmail.com>
2021-11-22 08:27:38 +0200

    Update firstDuplicate.ts

--- firstDuplicate/firstDuplicate.ts
+++ firstDuplicate/firstDuplicate.ts
@@ -1,5 +1,13 @@
 function firstDuplicate(a: number[]): number {
-    
+  let set = new Set();
+  for (let i = 0; i < a.length; i++) {
+    if (set.has(a[i])) {
+      return a[i];
+    } else {
+      set.add(a[i]);
+    }
+  }
+  return -1;
 }
 
 console.log(firstDuplicate([2, 1, 3, 5, 3, 2]));
