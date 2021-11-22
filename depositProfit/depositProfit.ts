7544e20a83c6655aac6e0727d22aa756e47efaba
Mohammed Dawood <mahmedtony73@gmail.com>
2021-11-22 06:16:40 +0200

    Update depositProfit.ts

--- depositProfit/depositProfit.ts
+++ depositProfit/depositProfit.ts
@@ -1,5 +1,16 @@
-function depositProfit(deposit: number, rate: number, threshold: number): number {
- 
+function depositProfit(
+  deposit: number,
+  rate: number,
+  threshold: number
+): number {
+  let years = 0;
+
+  while (deposit < threshold) {
+    deposit += (deposit * rate) / 100;
+    years++;
+  }
+
+  return years;
 }
 
-console.log(depositProfit(100, 20, 170))
+console.log(depositProfit(100, 20, 170));
