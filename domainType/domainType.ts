1b1832dbbd60a92c9829c1bc455e9fb6af1b65ae
Mohammed Dawood <mahmedtony73@gmail.com>
2021-11-22 06:49:59 +0200

    Update domainType.ts

--- domainType/domainType.ts
+++ domainType/domainType.ts
@@ -1,5 +1,23 @@
 function domainType(domains: string[]): string[] {
-
+  const domainType: string[] = [];
+  for (const domain of domains) {
+    const domainName = domain.split(".")[domain.split(".").length - 1];
+    console.log(domainName);
+    if (domainName === "com") {
+      domainType.push("commercial");
+    } else if (domainName === "org") {
+      domainType.push("organization");
+    } else if (domainName === "net") {
+      domainType.push("network");
+    } else if (domainName === "info") {
+      domainType.push("information");
+    } else {
+      domainType.push("unknown");
+    }
+  }
+  return domainType;
 }
 
-console.log(domainType(["en.wiki.org", "codefights.com", "happy.net", "code.info"]));
+console.log(
+  domainType(["en.wiki.org", "codefights.com", "happy.net", "code.info"])
+);
