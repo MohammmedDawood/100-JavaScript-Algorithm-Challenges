7af362cc9cf8e61e8e5e068ec548fad4ae1bcfdc
Mohammed Dawood <mahmedtony73@gmail.com>
2021-11-22 07:00:04 +0200

    Update electionWinners.ts

--- electionWinners/electionWinners.ts
+++ electionWinners/electionWinners.ts
@@ -1,5 +1,7 @@
 function electionsWinners(votes: number[], k: number): number {
-
+  let maxVotes = Math.max(...votes);
+  let winners = votes.filter((vote) => vote + k > maxVotes);
+  return winners.length;
 }
 
 console.log(electionsWinners([2, 3, 5, 2], 3));
